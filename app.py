4f3e2bd92dbd2dba77a6b28531aa702844490116
karndiy <karndiy@gmail.com>
2025-07-31 09:08:08 +0700

    up

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, flash
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'supersecretkey'  # สำหรับ flash message
 
 # Create upload folder if not exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
 def index():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    files = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if os.path.isfile(filepath):
+            stat = os.stat(filepath)
+            files.append({
+                'name': filename,
+                'size': stat.st_size,
+                'mtime': datetime.fromtimestamp(stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+            })
     return render_template('index.html', files=files)
 
 @app.route('/upload', methods=['POST'])
@@ -28,6 +39,16 @@ def upload_file():
 @app.route('/download/<filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+@app.route('/delete/<filename>', methods=['POST'])
+def delete_file(filename):
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        flash(f'ลบไฟล์ {filename} แล้ว')
+    else:
+        flash(f'ไม่พบไฟล์ {filename}')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
